597cf8de11f71b851329ca732f19ea48d5b8383e
Swapnil Siddharth <39430010+swapsid@users.noreply.github.com>
2025-06-20 13:26:10 +0100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://miniblog.nicholasly.com";
-export const SITE_TITLE = "Miniblog";
+export const SITE_URL = "https://swapsid.com";
+export const SITE_TITLE = "Swapnil Siddharth";
 export const SITE_DESCRIPTION =
-  "Miniblog is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.";
+  "Hello World";
 
-export const EMAIL = "hello@nicholasly.com";
+export const EMAIL = "swapsid01@gmail.com";
